2e6f14cd2c4a9d440f141865827247fab0d111eb
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2020-08-23 22:19:27 +0800

    Firebase Update 1

--- KnowYourself/app/src/main/java/com/example/knowyourself/MainActivity.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/MainActivity.java
@@ -11,10 +11,13 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- KnowYourself/app/src/main/java/com/example/knowyourself/SignInFragment.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/SignInFragment.java
@@ -1,18 +1,107 @@
 package com.example.knowyourself;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignInFragment extends Fragment {
+
+    //signIn
+    private Button btnSignIn;
+    private EditText eTextEmail;
+    private EditText eTextPassword;
+
+    private ProgressDialog mProgressDialog;
+    private FirebaseAuth mFirebaseAuth;
+
+    private InputMethodManager imm;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_signin,container,false);
+        final View view = inflater.inflate(R.layout.fragment_signin, container, false);
+
+        btnSignIn = (Button) view.findViewById(R.id.btn_signIn);
+        eTextEmail = (EditText) view.findViewById(R.id.editText_email);
+        eTextPassword = (EditText) view.findViewById(R.id.editText_password);
+        imm = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                String email = eTextEmail.getText().toString().trim();
+                String password = eTextPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    //email is empty
+                    Snackbar.make(view, "Please enter the email.", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    //password is empty
+                    Snackbar.make(view, "Please enter the password.", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    //email is invalid format
+                    Snackbar.make(view, "Please enter a valid email address.", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mProgressDialog = new ProgressDialog(getActivity());
+                mFirebaseAuth = FirebaseAuth.getInstance();
+
+                //if the fields all filled up
+                mProgressDialog.setMessage("Signing In...");
+                mProgressDialog.show();
+
+                mFirebaseAuth.signInWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                mProgressDialog.dismiss();
+                                if (task.isSuccessful()){
+                                    Toast.makeText(getActivity(),"Sign In Successfully",Toast.LENGTH_SHORT).show();
+                                    //back to home
+                                    startActivity(new Intent(getActivity(), MainActivity.class));
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        return view;
     }
 }
