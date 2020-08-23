a24524a53b5b19e7245241137ba44dc71579436f
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2020-08-23 22:27:35 +0800

    Firebase Update 2

--- KnowYourself/app/src/main/java/com/example/knowyourself/ProfileFragment.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/ProfileFragment.java
@@ -4,15 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
+
+    private TextView mTextViewEmail;
+    private Button btnLogOut;
+    FirebaseAuth mFirebaseAuth;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        mTextViewEmail = (TextView) view.findViewById(R.id.textViewUserEmail);
+        btnLogOut = (Button) view.findViewById(R.id.btn_logOut);
+
+        //check if user currently log in
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        if (mFirebaseAuth.getCurrentUser() != null) {
+            //get current user email and display on profile
+            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+            mTextViewEmail.setText(
+                    ""+ user.getEmail());
+
+            btnLogOut.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mFirebaseAuth.signOut();
+                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, new ViewPagerFragment()).addToBackStack(null).commit();
+                }
+            });
+        }
+        return view;
     }
 }
--- KnowYourself/app/src/main/java/com/example/knowyourself/RegisterFragment.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/RegisterFragment.java
@@ -1,18 +1,105 @@
 package com.example.knowyourself;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterFragment extends Fragment {
+
+    private EditText eTextEmailR;
+    private EditText eTextPasswordR;
+    private InputMethodManager imm;
+    private Button button;
+
+    private ProgressDialog mProgressDialog;
+    private FirebaseAuth mFirebaseAuth;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_register,container,false);
+        final View view = inflater.inflate(R.layout.fragment_register, container, false);
+        button = (Button) view.findViewById(R.id.btn_register);
+        eTextEmailR = (EditText) view.findViewById(R.id.editText_emailR);
+        eTextPasswordR = (EditText) view.findViewById(R.id.editText_passwordR);
+        imm = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                String emailR = eTextEmailR.getText().toString().trim();
+                String passwordR = eTextPasswordR.getText().toString().trim();
+
+                if(TextUtils.isEmpty(emailR)){
+                    //email is empty
+                    Snackbar.make(view, "Please enter the email.", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(passwordR)){
+                    //password is empty
+                    Snackbar.make(view, "Please enter the password.", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(emailR).matches())
+                {
+                    //email is invalid format
+                    Snackbar.make(view, "Please enter a valid email address.", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mProgressDialog = new ProgressDialog(getActivity());
+                mFirebaseAuth = FirebaseAuth.getInstance();
+
+                //if the fields all filled up
+                mProgressDialog.setMessage("Registering user...");
+                mProgressDialog.show();
+
+                mFirebaseAuth.createUserWithEmailAndPassword(emailR,passwordR)
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                mProgressDialog.dismiss();
+                                if (task.isSuccessful()) {
+                                    //if registration is complete
+                                    Toast.makeText(getActivity(), "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                                    fragmentTransaction.replace(R.id.fragment_container, new SignInFragment()).addToBackStack(null).commit();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
+        return view;
     }
 }
