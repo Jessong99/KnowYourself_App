d6726af84720e7b0e530571d5a6e42e6dcfd88b4
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2020-10-15 15:49:51 +0800

    Update

--- KnowYourself/app/src/main/java/com/example/knowyourself/HomeFragment.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/HomeFragment.java
@@ -4,17 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
+
+    private Button btnDISCTest;
+    FirebaseAuth mFirebaseAuth;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view =  inflater.inflate(R.layout.fragment_home,container,false);
+        //check if user currently log in
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
+        btnDISCTest = (Button) view.findViewById(R.id.btn_DISCTest);
+
+        if (mFirebaseAuth.getCurrentUser() != null) {
+            btnDISCTest.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(),"Success", Toast.LENGTH_SHORT).show();
+                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, new SignInFragment()).addToBackStack(null).commit();
+                }
+            });
+        }else{
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new SignInFragment()).addToBackStack(null).commit();
+
+        }
 
         return view;
     }
