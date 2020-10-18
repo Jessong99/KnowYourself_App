486dc8e5cfe7b28880662d57b4ab3eaffc01eeef
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2020-10-18 17:24:19 +0800

    Update

--- KnowYourself/app/src/main/java/com/example/knowyourself/DiscAdapter.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/DiscAdapter.java
@@ -1,6 +1,7 @@
 package com.example.knowyourself;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +14,20 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DiscAdapter extends RecyclerView.Adapter<DiscAdapter.MyViewHolder>{
 
     Context mContext;
     ArrayList<DISC> disc;
-    ArrayList<String> result = new ArrayList<String>();
-    Object[] results = new Object[100];
+    //ArrayList<String> result = new ArrayList<String>();
+    //Result r = new Result();
+   // Object[] results = new Object[100];
+    Map<Integer, String> map = new HashMap<Integer, String>();
+    //Shared Preferences
+    private SharedPreferences mPreferences;
+    private String spFileName = "com.example.sharedpreference" ;
 
     public DiscAdapter(Context c, ArrayList<DISC> list) {
         mContext = c;
@@ -42,27 +50,46 @@ public class DiscAdapter extends RecyclerView.Adapter<DiscAdapter.MyViewHolder>{
         holder.rBsel2.setText(disc.get(position).getSel2());
         holder.rBsel3.setText(disc.get(position).getSel3());
         holder.rBsel4.setText(disc.get(position).getSel4());
-        for (int j = 0; j < result.size(); j++) {
-            result.add(null);
-        }
+
+        //final String hi = mPreferences.getString(spFileName,"sel1");
         holder.mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 switch (i) {
                     case R.id.radioButton:
                         String type = disc.get(position).getType1();
-                        Toast.makeText(mContext,type, Toast.LENGTH_SHORT).show();
-                        result.set(position,type);
-                        results[position] = type;
+                        //Toast.makeText(mContext,type, Toast.LENGTH_SHORT).show();
+                        //result.set(position,type);
+                        //results[position] = type;
+                        //r.setType(type);
+                        map.put(position,type);
+                        //Toast.makeText(mContext,hi, Toast.LENGTH_SHORT).show();
+                        String print =  String.valueOf(map.values());
+                        Toast.makeText(mContext,print,Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.radioButton2:
-                        Toast.makeText(mContext,disc.get(position).getType2(), Toast.LENGTH_SHORT).show();
+                        String type2 = disc.get(position).getType2();
+                        map.put(position,type2);
+                        String print2 =  String.valueOf(map.values());
+                        Toast.makeText(mContext,print2,Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(mContext,type2, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(mContext,map.get(position), Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.radioButton3:
-                        Toast.makeText(mContext,disc.get(position).getType3(), Toast.LENGTH_SHORT).show();
+                        String type3 = disc.get(position).getType3();
+                        map.put(position,type3);
+                        String print3 =  String.valueOf(map.values());
+                        Toast.makeText(mContext,print3,Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(mContext,type3, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(mContext,map.get(position), Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.radioButton4:
-                        Toast.makeText(mContext,disc.get(position).getType4(), Toast.LENGTH_SHORT).show();
+                        String type4 = disc.get(position).getType4();
+                        map.put(position,type4);
+                        String print4 =  String.valueOf(map.values());
+                        Toast.makeText(mContext,print4,Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(mContext,type4, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(mContext,map.get(position), Toast.LENGTH_SHORT).show();
                         break;
                     default:
                         Toast.makeText(mContext,"Default", Toast.LENGTH_SHORT).show();
--- KnowYourself/app/src/main/java/com/example/knowyourself/DiscFragment.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/DiscFragment.java
@@ -1,5 +1,6 @@
 package com.example.knowyourself;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,21 +25,37 @@ import java.util.ArrayList;
 
 public class DiscFragment extends Fragment {
 
+    //RecyclerView
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
     private ArrayList<DISC> mList;
+
+    //Firebase
     private FirebaseAuth mFirebaseAuth;
     private DatabaseReference mDatabaseReference;
 
     private Button btnSubmitTest;
+    //Shared Preferences
+    private SharedPreferences mPreferences;
+    private String spFileName = "com.example.sharedpreference" ;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view =  inflater.inflate(R.layout.fragmemt_disc,container,false);
 
+        //initialize shared preferences
+        mPreferences = this.getActivity().getSharedPreferences(spFileName, getContext().MODE_PRIVATE);
+        //initialization of editor
+        SharedPreferences.Editor spEditor = mPreferences.edit();
+        //put key-value pair
+        spEditor.putString("sel1","hi");
+        //save the preferences
+        spEditor.apply();
+
+        //Set Up recyclerView
         recyclerView = (RecyclerView)view.findViewById(R.id.disc_recycler_view);// use a linear layout manager
         layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
@@ -55,13 +72,16 @@ public class DiscFragment extends Fragment {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 mList = new ArrayList<DISC>();
 
+                //get ques children
                 for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
                     DISC d = new DISC();
                     String ques = dataSnapshot1.getKey();
                     int counter = 1;
+                    //get all selection & type in ques
                     for (DataSnapshot dataSnapshot2 : dataSnapshot1.getChildren()) {
                         String sel = (String) dataSnapshot2.child("selection").getValue();
                         String type = (String) dataSnapshot2.child("type").getValue();
+                        //save into arrayList
                         switch (counter){
                             case 1:
                                 d.setSel1(sel);
