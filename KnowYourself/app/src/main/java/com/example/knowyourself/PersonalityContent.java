6c09a4242431d97445ad70977ff269edd6ba13bc
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2020-10-23 14:55:59 +0800

    Update

--- KnowYourself/app/src/main/java/com/example/knowyourself/PersonalityContent.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/PersonalityContent.java
@@ -1,8 +1,29 @@
 package com.example.knowyourself;
 
 public class PersonalityContent {
-    private PersonalityContent(){
+    private String title, content;
+
+    public PersonalityContent(){
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public PersonalityContent(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
--- KnowYourself/app/src/main/java/com/example/knowyourself/PersonalityContentAdapter.java
+++ KnowYourself/app/src/main/java/com/example/knowyourself/PersonalityContentAdapter.java
@@ -1,4 +1,5 @@
 package com.example.knowyourself;
 
 public class PersonalityContentAdapter {
+
 }
