ad60c00a9bb9ef723eb41363b4a308e79a859fce
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2020-10-29 00:11:08 +0800

    MyFeed

---
+++ KnowYourself/app/src/main/java/com/example/knowyourself/MyFeed.java
@@ -0,0 +1,47 @@
+package com.example.knowyourself;
+
+public class MyFeed {
+    private String timeStamp, title, article, fileName;
+
+    public MyFeed() {
+    }
+
+    public MyFeed(String timeStamp, String title, String article, String fileName) {
+        this.timeStamp = timeStamp;
+        this.title = title;
+        this.article = article;
+        this.fileName = fileName;
+    }
+    
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArticle() {
+        return article;
+    }
+
+    public void setArticle(String article) {
+        this.article = article;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
