8a4b1b5a7ff4838d63981e46f0682dc829dce84e
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2020-11-08 23:02:37 +0800

    Update

---
+++ KnowYourself/app/src/main/java/com/example/knowyourself/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.knowyourself;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 500);
+
+    }
+}
